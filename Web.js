dece614b6ca9eaab654bf2164c9987084970880a
RUCuwu <afiji.x04@gmail.com>
2024-08-18 10:06:25 -0500

    ain

--- Web.js
+++ Web.js
@@ -1,5 +1,46 @@
 console.log(location.hash);
 
 window.addEventListener('hashchange', function() {
-    console
-})
+    var contentDiv = this.document.getElementById("app");
+    contentDiv.innerHTML = this.location.hash;
+});
+
+
+function loadContent() {
+    var contentDiv = document.getElementById('app');
+    contentDiv.innerHTML = location.hash;
+}
+
+
+  window.addEventListener('hashchange', function (){
+    loadContent();
+});
+
+// loadContent();
+
+function getContent(fragmentId) {
+    var pages = {
+        home: 'This is the Home page. Welcome to my site.',
+        about: 'This page will describe what my site is about',
+        contact: 'Contact me on this page if you have any questions'
+    };
+    return pages[fragmentId];
+} 
+
+function loadContent() {
+    var contentDiv = document.getElementById('app');
+    fragmentId = location.hash.substr(1);
+    contentDiv.innerHTML = getContent(fragmentId);
+}
+
+if(!location.hash) {
+    location.hash = '#home';
+}
+
+loadContent();
+
+window.addEventListener('hashchange', loadContent)
+
+
+
+
